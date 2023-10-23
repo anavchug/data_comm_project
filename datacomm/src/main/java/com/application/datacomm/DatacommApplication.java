f71fbbd8aab654f8f3ae76ab6b69be69700469e9
Anav Chug <ac32809@georgiasouthern.edu>
2023-10-23 18:14:03 -0400

    Spring Project Initialized

---
+++ datacomm/src/main/java/com/application/datacomm/DatacommApplication.java
@@ -0,0 +1,13 @@
+package com.application.datacomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacommApplication.class, args);
+	}
+
+}
