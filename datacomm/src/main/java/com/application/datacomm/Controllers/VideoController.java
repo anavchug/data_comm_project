1914dee2c930976d56e5deb6b4a913497b66ddca
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-10-31 17:07:32 -0400

    Created basic class with /video route

---
+++ datacomm/src/main/java/com/application/datacomm/Controllers/VideoController.java
@@ -0,0 +1,21 @@
+package com.application.datacomm.Controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.application.datacomm.Video;
+
+
+@RestController
+public class VideoController {
+
+
+    @GetMapping("/video")
+    public Video video(){
+
+       
+
+    }
+
+
+    
+}
