c09b38f1211d0cb3fc7ecf81d490e86ee79208a7
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-10-31 17:28:28 -0400

    added parseJson method to YoutubeApi class to parse and return Video record for api

--- datacomm/src/main/java/com/application/datacomm/YoutubeApi.java
+++ datacomm/src/main/java/com/application/datacomm/YoutubeApi.java
@@ -5,6 +5,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class YoutubeApi {
 
 
@@ -18,6 +21,40 @@ public class YoutubeApi {
         apiUrl+= "&maxResults=" + Integer.toString(maxResults);
         apiUrl+= "&regionCode=" + region;
         apiUrl+= "&key=" + this.apiKey;
+    }
+
+    public static Video parseJson(String jsonData){
+
+        String videoStub = "https://www.youtube.com/watch?v=";
+        
+        JSONObject json = new JSONObject(jsonData);
+        JSONArray items = json.getJSONArray("items");
+
+        Item[] videoItems = new Item[items.length()];
+
+
+        for(int i = 0; i < items.length(); i++){
+
+            JSONObject video = items.getJSONObject(i);
+            JSONObject videoSnip = video.getJSONObject("snippet");
+            JSONObject thumb = videoSnip.getJSONObject("thumbnails");
+            JSONObject size = thumb.getJSONObject("medium");
+
+            
+            String videoUrl = videoStub + video.getString("id");
+            String videoTitle = videoSnip.getString("title");
+            String videoDesc = videoSnip.getString("description");
+            String thumbUrl = size.getString("url");
+
+
+            videoItems[i] = new Item(videoTitle, videoDesc, videoUrl, thumbUrl);
+
+
+       }
+
+       return new Video(videoItems);
+
+
     }
 
 
