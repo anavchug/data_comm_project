6088135fddaadd22609219fec890902a6e78f68c
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-11-02 15:49:11 -0400

    Wrote some basic javascript to call and load data from api

--- datacomm/src/main/resources/static/application.js
+++ datacomm/src/main/resources/static/application.js
@@ -1,1 +1,45 @@
-console.log("Anav is a dick");
+
+
+var request = new XMLHttpRequest();
+request.open("GET","http://localhost:8080/video");
+request.addEventListener("load",handleData)
+request.send();
+
+
+function handleData(){
+
+    if(this.status == 200){
+
+        let data = JSON.parse(this.responseText);
+
+        let source = document.getElementById("videos");
+
+        data.forEach((item) =>{
+
+            let holder = document.createElement("div");
+            let title = document.createElement("h2");
+            let img = document.createElement('img');
+
+            let url = document.createElement("h2");
+            let link = document.createElement('a');
+
+            link.setAttribute('href', item.url);
+            //link.textContent = "Link";
+            //url.appendChild(link);
+            
+
+            title.textContent = item.title;
+            img.setAttribute("src",item.thumbUrl);
+            link.appendChild(img);
+
+            holder.appendChild(title);
+            holder.appendChild(link);
+            //holder.appendChild(img);
+            //holder.appendChild(url);
+            
+
+            source.appendChild(holder);
+
+        })
+    }
+}
