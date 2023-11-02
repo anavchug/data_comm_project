3572976b39767fa16dd03b0d724ffce0ba3de38b
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-11-01 22:43:16 -0400

    Rewrote Video class file to be simplier

--- datacomm/src/main/java/com/application/datacomm/RestAPI/Video.java
+++ datacomm/src/main/java/com/application/datacomm/RestAPI/Video.java
@@ -1,4 +1,38 @@
 package com.application.datacomm.RestAPI;
 
 
-public record Video(Item[] videos){}
+public class Video{
+
+    private String title, url, thumbUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public void setThumbUrl(String thumbUrl) {
+        this.thumbUrl = thumbUrl;
+    }
+
+    
+
+    
+}
+
+
+
