1c8db6af6f40221626b724de277e2cd68cb99a9d
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-11-01 22:48:24 -0400

    changed method signiture of videocontroller to return List<Video>

--- datacomm/src/main/java/com/application/datacomm/Controller/VideoController.java
+++ datacomm/src/main/java/com/application/datacomm/Controller/VideoController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.application.datacomm.RestAPI.Video;
 import com.application.datacomm.RestAPI.YoutubeApi;
 
+import java.util.List;
+
 
 @RestController
 public class VideoController {
 
 
     @GetMapping("/video")
-    public Video video() throws Exception{
+    public List<Video> video() throws Exception{
 
        YoutubeApi request = new YoutubeApi("US",10);
        String result = request.makeRequest();
