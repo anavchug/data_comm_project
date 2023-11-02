902be63c8a1b20cd31ec69d5c55210c19a8e096d
LazyOverLord <44475186+LazyOverLord@users.noreply.github.com>
2023-11-02 15:09:13 -0400

    setup basic route /

---
+++ datacomm/src/main/java/com/application/datacomm/Controller/ContentController.java
@@ -0,0 +1,29 @@
+package com.application.datacomm.Controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.MediaType;
+
+@RestController
+public class ContentController {
+
+    @GetMapping("/")
+    public ResponseEntity<InputStreamResource> main() throws IOException {
+        ClassPathResource htmlFile = new ClassPathResource("templates/index.html");
+        String htmlContent = new String(htmlFile.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(new InputStreamResource(new ByteArrayInputStream(htmlContent.getBytes())));
+    }
+    
+}
